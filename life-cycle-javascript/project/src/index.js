276b9f3f210aed878905a8cbb48317b0e37b4807
eddyzera <esilva644@gmail.com>
2023-01-24 11:03:06 -0300

    feat: configure terminal interface

--- life-cycle-javascript/project/src/index.js
+++ life-cycle-javascript/project/src/index.js
@@ -1,3 +1,30 @@
+import DraftLog from 'draftlog'
+import chalk from 'chalk'
+import readline from 'readline'
+import chalkTable from 'chalk-table'
 import database from '../database.json'
 
-console.log('database', database)
+DraftLog(console).addLineListener(process.stdin)
+
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: "id", name: chalk.cyan("ID") },
+    { field: "vehicles", name: chalk.magenta("Vehicles") },
+    { field: "kmTraveled", name: chalk.red("Km Traveled") },
+    { field: "from", name: chalk.green("From") },
+    { field: "to", name: chalk.white("To") },
+  ]
+}
+
+const table = chalkTable(options, database)
+const print = console.draft(table)
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+terminal.question('Qual é o seu nome ?', msg => {
+  console.log(msg)
+})
